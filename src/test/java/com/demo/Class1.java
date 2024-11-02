3e108c55810903cdc73c90b0f84fc59ccda2019e
NagarajuThulluru <thullurunagaraju9@gmail.com>
2024-11-02 11:11:23 +0530

    first commit

---
+++ src/test/java/com/demo/Class1.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Class1 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		int d=a-b;
+		System.out.println("addition of a and b values are:"+c);
+	}
+
+}
